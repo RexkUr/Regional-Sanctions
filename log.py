b383d21eac950a97483d2727433fe4bdba6a37d4
RexkUr <92269279+RexkUr@users.noreply.github.com>
2024-10-30 00:04:01 -0400

    set up logger

---
+++ log.py
@@ -0,0 +1,40 @@
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# Set up logging
+logging.basicConfig(
+    filename='file_changes.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s'
+)
+
+# Create a handler class
+class ChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        logging.info(f'Modified: {event.src_path}')
+
+    def on_created(self, event):
+        logging.info(f'Created: {event.src_path}')
+
+    def on_deleted(self, event):
+        logging.info(f'Deleted: {event.src_path}')
+
+    def on_moved(self, event):
+        logging.info(f'Moved: {event.src_path} to {event.dest_path}')
+# Change this to your target directory
+# Set up the observer
+if __name__ == "__main__":
+    path = "C:\\Users\\Red RexkUr\\AppData\\Roaming\\ATLauncher\\instances\\lololol\\mods"  
+    event_handler = ChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
